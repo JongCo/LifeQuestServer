08475a454a67849643653a085790a6372717aae7
JongCo <xn1128@gmail.com>
2024-02-22 15:17:05 +0900

    Update git ignore

--- src/main/resources/db/initiate.sql
+++
@@ -1,6 +0,0 @@
--- User Table Insert --
-INSERT INTO life_quest_dev.`user` (id, user_name, pass_word) VALUES('7adb0ca0-d05f-11ee-a506-0242ac120002', 'jongco', '1234');
-
--- User Roll Table Insert --
-INSERT INTO life_quest_dev.user_entity_roles (user_entity_id, roles) VALUES('7adb0ca0-d05f-11ee-a506-0242ac120002', 'USER');
-
