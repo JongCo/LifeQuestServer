24162f448287f16c3e28bd413d06cfc038aa4c21
JongCo <xn1128@gmail.com>
2024-03-04 02:09:03 +0900

    quest를 get할때 요청자 user id를 통해 응답

--- src/main/kotlin/jongco/jongco/lifeQuest/api/quest/QuestController.kt
+++ src/main/kotlin/jongco/jongco/lifeQuest/api/quest/QuestController.kt
@@ -1,22 +1,33 @@
 package jongco.jongco.lifeQuest.api.quest
 
+import jakarta.servlet.http.HttpServletRequest
+import jongco.jongco.lifeQuest.api.user.UserEntity
+import jongco.jongco.lifeQuest.security.jwt.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.SessionAttribute
+import java.security.Principal
 import java.util.*
 
 @RestController
 @RequestMapping("/quest")
 class QuestController (
-    @Autowired val questService: QuestService
+    @Autowired val questService: QuestService,
+    @Autowired val jwtTokenProvider: JwtTokenProvider
 ) {
     @GetMapping("/get")
-    fun getQuestsByOwner(@RequestBody getQuestsByUuidRequestDto: GetQuestsByUuidRequestDto): QuestsDto {
-        return questService.getQuestsByOwner(getQuestsByUuidRequestDto.uuid)
+    fun getQuestsByOwner(
+        authentication: Authentication,
+    ): QuestsDto {
+        val requestUserId: String? = (authentication.details as MutableMap<String, String>)["id"]
+        return questService.getQuestsByOwner(UUID.fromString(requestUserId))
     }
 
     @PostMapping("/create")
