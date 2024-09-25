14ff70459075c37805e4389c8b970b25156256f5
jongco <xn1128@gmail.com>
2024-09-26 03:06:33 +0900

    SpringBootApplication class 이름 변경

--- src/main/kotlin/jongco/jongco/lifeQuest/LifeQuestServerApp.kt
+++ src/main/kotlin/jongco/jongco/lifeQuest/LifeQuestServerApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringPracticeApplication
+class LifeQuestServerApp
 
 fun main(args: Array<String>) {
-	runApplication<SpringPracticeApplication>(*args)
+	runApplication<LifeQuestServerApp>(*args)
 }
